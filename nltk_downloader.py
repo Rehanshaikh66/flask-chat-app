9f624e1a66946967e3b736b50629ed1bbc30cb1a
Shaikh Rehan Rafiq <rehanshaikh02269@gmail.com>
2025-06-14 16:02:17 +0530

    fix: download vader_lexicon during build via render.yaml

--- nltk_downloader.py
+++ nltk_downloader.py
@@ -1,3 +1,7 @@
 import os
 import nltk
-nltk.download('vader_lexicon', download_dir=os.environ.get('NLTK_DATA'))
+
+# Ensure the NLTK_DATA env var is used
+nltk_data_path = os.environ.get("NLTK_DATA", "/opt/render/nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.download("vader_lexicon", download_dir=nltk_data_path)
