37c9ebdc990515e4cfc868c06cf7696102971dfd
Rehanshaikh66 <109944168+Rehanshaikh66@users.noreply.github.com>
2025-06-14 12:30:50 +0530

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-
-uri = "mongodb+srv://rehanshaikh02269:REHAN123@cluster0.nuvotwa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = pymongo.MongoClient(uri)
-print(client.list_database_names())
