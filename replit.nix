c8b870b9fd085cb9e25370553c9a3292f55f4cf4
Shaikh Rehan Rafiq <rehanshaikh02269@gmail.com>
2025-06-15 21:58:13 +0530

    ✅ Deployed version: Replit-ready Flask app with MongoDB Atlas & Sentiment

--- replit.nix
+++ replit.nix
@@ -2,10 +2,11 @@
   deps = [
     pkgs.python3
     pkgs.python3Packages.pip
-    pkgs.python3Packages.flask
-    pkgs.python3Packages.nltk
-    pkgs.python3Packages.flask_socketio
-    pkgs.python3Packages.python_dotenv
-    pkgs.python3Packages.pymongo
+    pkgs.python3Packages."flask"
+    pkgs.python3Packages."flask-socketio"
+    pkgs.python3Packages."python-dotenv"
+    pkgs.python3Packages."pymongo"
+    pkgs.python3Packages."nltk"
   ];
 }
+
